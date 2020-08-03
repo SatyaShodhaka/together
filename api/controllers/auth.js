f87efd20f3a94efca998418aaad3c5ee0df02dac
SatyaShodhaka <satyashodhaka66@gmail.com>
2020-08-04 02:57:48 +0530

    user signup and login created

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
 
@@ -30,8 +32,50 @@ exports.signup = (req, res) => {
     });
 };
 
-exports.signout = (req, res) => {
+exports.login = (req, res) => {
+    //do validation
+    const validationErrs = validationResult(req);
+    var validationRes = {errs:[]};
+
+    for (i in validationErrs.errors) {
+        validationRes.errs.push({msg:validationErrs.errors[i].msg,param:validationErrs.errors[i].param});
+      }
+
+    if(!validationErrs.isEmpty()) {
+        return res.status(422).json({
+            validationRes
+        })
+    }
+
+    //authenticate
+    const {email, password} = req.body;
+
+    User.findOne({email}, (err, user) => {
+        if(err || !user){
+            return res.status(400).json({
+                error: "User doesn't exist, please sign up."
+            });
+        }
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        };
+        //create and assign a token on succesful login
+        const token = jwt.sign({_id: user._id},process.env.SECRET, { expiresIn: '1h' })
+        //put token in cookie
+        res.cookie("token", token);
+
+        //send response to front end
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}})
+    });
+
+}
+
+exports.logout = (req, res) => {
+    res.clearCookie("token"),
     res.json({
-        message: "User signout"
+        message: "User loggedout"
     });
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var router = express.Router()
-const { check, validationResult } = require('express-validator')
-//import controllers
-const {signup,signout} = require('../controllers/auth')
+const express = require('express');
+const router = express.Router()
+//need express import to create Router
 
-router.get("/signout", signout);
+const { check, validationResult } = require('express-validator')
+
+
+//import controllers
+const {signup,login,logout} = require('../controllers/auth')
 
 router.post('/signup', [
     check('email').isEmail().withMessage('Not a valid email id'),
     check('password').isLength({min:8}).withMessage('Password must be atleast 8 characters')
 ], signup)
 
+router.post('/login', [
+    check('email').isEmail().withMessage('Not a valid email id'),
+    check('password').isLength({min:8}).withMessage('Password must be atleast 8 characters')
+], login)
+
+router.get("/logout", logout);
 
 module.exports = router;
